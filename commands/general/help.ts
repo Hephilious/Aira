4d12b2927a36ab02673d1b5e3fada6bef0459733
Luminary <chamb343@gmail.com>
2022-11-02 18:26:07 -0600

    made paginator

--- commands/general/help.ts
+++ commands/general/help.ts
@@ -1,11 +1,58 @@
+import { Manager } from "erela.js";
+import { CommandClient, ComponentInteraction, Message, User } from "eris";
+import { Paginator } from "../../LumDux/Paginator";
+let user: User;
+let deleteMessage: boolean = false;
+let embeds: Array<object> = [
+  {
+    title: "Category Change",
+    author: {
+      name: "Luminary",
+      icon_url: "https://i.postimg.cc/gJc4pZb3/Luminary.png",
+    },
+    color: 0xf76233,
+    fields: [
+      {
+        name: "$play <url || search> | Music",
+        value:
+          "plays a song of your choice by using a youtube url of your choice or a youtube search as an argument" +
+          "Use `$play help` to get more info on the command.",
+      },
+      {
+        name: "",
+      },
+    ],
+  },
+  {
+    title: "test2",
+    description: "hello2",
+  },
+  {
+    title: "test3",
+    description: "hello3",
+  },
+];
+let helpPaginator = new Paginator(
+  3,
+  embeds,
+  "prev_embed_help",
+  "next_embed_help"
+);
+
 module.exports = {
   name: "help",
   description: "retrieve a list of commands or get usage on some commands",
   argsRequired: false,
-  guildOnly: false,
+  guildOnly: true,
   usage: "$help",
   alias: [],
-  run: async (client, message, args, Manager) => {
+  run: async (
+    client: CommandClient,
+    message: Message,
+    args,
+    Manager: Manager
+  ) => {
+    user = message.author;
     const fs = require("fs");
     let cmd_test = false;
     if (args[0]) {
@@ -43,24 +90,64 @@ module.exports = {
           `${message.author.mention} I don't seem to have a command called "${args[0]}"`
         );
     } else {
-      let embed = {
-        title: "Help",
-        description: "Here's a list of commands",
-        color: 0xf76233,
-        fields: [
-          {
-            name: "Music",
-            value: "Use `$help music` to show the commands for Music and Audio",
-            inline: true,
-          },
-        ],
-        author: {
-          name: "Luminary",
-          icon_url: "https://i.ibb.co/0KJ0fT3/Luminary.png",
-        },
-      };
+      let embeds: Array<Object> = [];
+      for (let i = 0; i < 4; i++) {
+        embeds.push({
+          description: "hello",
+        });
+      }
+
       const user = await client.getDMChannel(message.author.id);
-      client.createMessage(user.id, { embed: embed });
+      message.channel.createMessage(
+        helpPaginator.getRow(helpPaginator.currentPage.toString())
+      );
     }
   },
+  button_ids: [
+    {
+      name: "next_embed_help",
+      run: async (Interaction: ComponentInteraction) => {
+        helpPaginator.currentPage++;
+        const id = Interaction.member.id;
+        helpPaginator.pages[id] = helpPaginator.pages[id] || 0;
+
+        const embed: Object = helpPaginator.embeds[helpPaginator.pages[id]];
+
+        let filter = (i: ComponentInteraction) => i.member.id === user.id;
+        await Interaction.acknowledge();
+        if (deleteMessage) {
+          Interaction.deleteOriginalMessage();
+        } else {
+          deleteMessage = true;
+        }
+        if (filter(Interaction)) {
+          Interaction.createFollowup(
+            helpPaginator.getRow(helpPaginator.currentPage.toString())
+          );
+        }
+      },
+    },
+    {
+      name: "prev_embed_help",
+      run: async (Interaction: ComponentInteraction) => {
+        helpPaginator.currentPage--;
+        const id = Interaction.member.id;
+        helpPaginator.pages[id] = helpPaginator.pages[id] || 0;
+
+        const embed: Object = helpPaginator.embeds[helpPaginator.pages[id]];
+        await Interaction.acknowledge();
+        if (deleteMessage) {
+          Interaction.deleteOriginalMessage();
+        } else {
+          deleteMessage = true;
+        }
+        let filter = (i: ComponentInteraction) => i.member.id === user.id;
+        if (filter(Interaction)) {
+          Interaction.createFollowup(
+            helpPaginator.getRow(helpPaginator.currentPage.toString())
+          );
+        }
+      },
+    },
+  ],
 };
